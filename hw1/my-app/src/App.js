d2e43cc3b8d7385c86cba2e1a2d69196fcc87512
umralieva <111286266+umralieva@users.noreply.github.com>
2022-11-13 02:10:45 +0600

    Add files via upload

---
+++ hw1/my-app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function AboutUs(){
+  return(
+    <div>
+    <h1>AboutUs</h1>
+  </div>
+  )
+}
+
+function Portfolio(){
+  return(
+    <div>
+    <h1>Portfolio</h1>
+  </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div className="App">
+<AboutUs/>
+      Homework-1
+<Portfolio/>
+    </div>
+  );
+}
+
+export default App;
